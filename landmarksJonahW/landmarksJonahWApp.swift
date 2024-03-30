ca6eb363fbfff090280372587530781cefdc837c
jonahwhitney11 <jwhitney@students.mchenry.edu>
2024-03-30 14:39:21 -0500

    Initial Commit

---
+++ landmarksJonahW/landmarksJonahWApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksJonahWApp.swift
+//  landmarksJonahW
+//
+//  Created by Jonah Whitney on 3/30/24.
+//
+
+import SwiftUI
+
+@main
+struct landmarksJonahWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
