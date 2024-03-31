369ba1ee45eb0ca6ca50dac8f10461df749f5354
jonahwhitney11 <jwhitney@students.mchenry.edu>
2024-03-31 14:33:45 -0500

    Landmarks tutorial app

---
+++ landmarksJonahW/Views/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  landmarksJonahW
+//
+//  Created by Jonah Whitney on 3/30/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage(image: Image("turtlerock"))
+}
